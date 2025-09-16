191b5c8e67ef1796f182331bdfda8ff15f54830c
Aleksey-Golub <alekseigolub.ag@gmail.com>
2025-09-16 15:47:04 +0200

    add skill view connectors

--- Assets/CodeBase/UI/Windows/Views/SkillsTree/SkillsTreeScreenView.cs
+++ Assets/CodeBase/UI/Windows/Views/SkillsTree/SkillsTreeScreenView.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using CodeBase.Data.Skills;
 using CodeBase.StaticData.SkillsData;
 using TMPro;
@@ -13,12 +14,15 @@ namespace CodeBase.UI.Windows.Views
         [SerializeField] private Button _closeButton;
         
         [SerializeField] private SkillView _skillViewPrefab;
+        [SerializeField] private GameObject _connectionPrefab;
         [SerializeField] private RectTransform _parent;
         
         [SerializeField] private TextMeshProUGUI _skillPointsText;
         [SerializeField] private Button _addSkillPointsButton;
         [SerializeField] private SelectedSkillPanel _selectedSkillPanel;
 
+        private readonly List<GameObject> _connectors = new();
+        
         public Dictionary<SkillId, ISkillView> Views { get; private set; } = new();
         public ISelectedSkillPanel SelectedSkillPanel => _selectedSkillPanel;
         
@@ -46,6 +50,15 @@ namespace CodeBase.UI.Windows.Views
                 SkillView view = CreateSkillView(skillConfig);
                 view.SetSelected(false);
                 Views[skillConfig.Id] = view;
+
+                foreach (SkillId linkedSkill in skillConfig.LinkedSkills)
+                {
+                    if (Views.ContainsKey(linkedSkill))
+                        continue;
+                    
+                    var connector = CreateConnection(skillConfig.SkillTreePosition, skillGraph.First(c => c.Id == linkedSkill).SkillTreePosition);
+                    _connectors.Add(connector);
+                }
             }
         }
 
@@ -59,13 +72,34 @@ namespace CodeBase.UI.Windows.Views
             return view;
         }
         
+        private GameObject CreateConnection(Vector2 start, Vector2 end)
+        {
+            GameObject connector = Instantiate(_connectionPrefab, _parent);
+            var rectTransform = connector.GetComponent<RectTransform>();
+            rectTransform.SetSiblingIndex(0);
+
+            Vector2 direction = end - start;
+            float distance = direction.magnitude;
+
+            rectTransform.sizeDelta = new Vector2(distance, rectTransform.sizeDelta.y);
+            rectTransform.anchoredPosition = (start + direction / 2f);
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rectTransform.localRotation = Quaternion.Euler(0, 0, angle);
+
+            return connector;
+        }
+        
         private void OnCloseClicked()
         {
             CloseClicked?.Invoke();
             
             foreach (ISkillView view in Views.Values) 
                 view.Destroy();
-            
+
+            foreach (GameObject connector in _connectors) 
+                Destroy(connector);
+
             Views.Clear();
         }
 
